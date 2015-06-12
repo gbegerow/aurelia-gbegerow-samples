736a6b5c6de5207f16b6ed75c9b698b663ae0602
Georg Begerow <gbegerow@googlemail.com>
2015-06-12 16:29:52 +0200

    flickr verschiedene queries, progress + throttle

--- src/flickr.js
+++ src/flickr.js
@@ -1,11 +1,15 @@
-import {inject} from 'aurelia-framework';
+import {inject,bindable} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 
 @inject(HttpClient)
 export class Flickr{
   heading = 'Flickr';
+
+  @bindable queryrunning = true;
+
   images = [];
-  url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=bonn,japanischer,garten&tagmode=all&format=json';
+  //url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=bonn,japanischer,garten&tagmode=all&format=json';
+  url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=paderquellen&tagmode=all&format=json';
 
   constructor(http){
     this.http = http;
@@ -13,7 +17,12 @@ export class Flickr{
 
   activate(){
     return this.http.jsonp(this.url).then(response => {
-      this.images = response.content.items;
+      // throttle 2s
+      //setTimeout(()=> { // how to do it with promise? Should be cleaner
+        this.queryrunning = false;
+        this.images = response.content.items;
+      //}, 2000);
+      
     });
   }
 }
